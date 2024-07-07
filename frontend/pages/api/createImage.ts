dfc18da5ed16b5908bdbf17e31d8d247bf992596
Architsharma7 <architsharma7911@gmail.com>
2024-07-07 10:52:43 +0200

    feat: added apis for chat, run and create

---
+++ frontend/pages/api/createImage.ts
@@ -0,0 +1,26 @@
+import { createImage } from "@/utils/openAIfunctions";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method != "POST") return res.status(405).send("Method Not Allowed");
+
+  const { prompt } = req.body;
+
+  try {
+    const result = await createImage(prompt);
+    console.log(result);
+
+    // we'll have to see if we want to run the Thread and produce the output
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json({ message: "Result undefined" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error });
+  }
+}
